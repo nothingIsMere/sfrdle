f4a353b532f32642d71fadb598bb7ce7e7779864
Joel <joel.knoll1@gmail.com>
2022-09-05 22:11:30 -0400

    Make future guess boxes unwriteable.

--- script.js
+++ script.js
@@ -3,18 +3,13 @@ const currentWordArray = Array.from(currentWord);
 let  masterGuessList = [];
 const letterboxNodeList = document.querySelectorAll(".letterbox");
 let letterboxArray = Array.from(letterboxNodeList);
-// const keyNodeList = document.querySelectorAll(".key");
-// let keyArray = Array.from(keyNodeList); 
 const acceptableKeys = ["A", "B", "C", "D", "E", "F", "G",
 "H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
 "t","u","v","w","x","y","z","Enter", "Backspace",];
 let testWordArray = [];
 let offLimitsCount = 0;
-
-// for(let j = 0; j < keyArray.length; j++){
-//   console.log(keyArray[j].id); 
-// }
+let letterCount = 0;  
 
 for(let i = 0; i < letterboxNodeList.length; i++){
   
@@ -32,6 +27,8 @@ window.addEventListener("keydown", (e) => {
   
   else if(e.key === "Backspace"){
 
+    letterCount -= 1; 
+
     if(masterGuessList.length === offLimitsCount){
       return; 
     }
@@ -42,16 +39,20 @@ window.addEventListener("keydown", (e) => {
   
   }
   
-  else if(e.key === "Enter"){
-      
-      offLimitsCount = masterGuessList.length;
+  else if(e.key === "Enter"){ 
 
-      if ((masterGuessList.length)%5 !== 0 || masterGuessList.length === 0){
-        
-        alert("Not enough letters");
-        return;
+    if(masterGuessList.length%5 === 0 & masterGuessList.length > 0){
+      letterCount = 0; 
+    }
       
-      }
+    offLimitsCount = masterGuessList.length;
+
+    if((masterGuessList.length)%5 !== 0 || masterGuessList.length === 0){
+      
+      alert("Not enough letters");
+      return;
+    
+    }
     else{
       
       testWordArray = [];
@@ -85,6 +86,7 @@ window.addEventListener("keydown", (e) => {
           let currentKey = document.getElementById(`${testWordArray[i]}`);
           currentKey.style.backgroundColor = "#c9b458";
           currentKey.style.color = "white";
+
         }
         else{
           
@@ -96,6 +98,7 @@ window.addEventListener("keydown", (e) => {
           let currentKey = document.getElementById(`${testWordArray[i]}`);
           currentKey.style.backgroundColor = "#787c7e";
           currentKey.style.color = "white";
+
         }
       }
     }
@@ -103,11 +106,16 @@ window.addEventListener("keydown", (e) => {
       
 
   else{
-    
+
+    if(letterCount >= 5){
+      return; 
+    }
+
     masterGuessList.push(e.key);
     letterboxArray[masterGuessList.length - 1].textContent = e.key.toUpperCase();
     letterboxArray[masterGuessList.length - 1].style.border = "2px solid #777";
-  
+    letterCount += 1; 
+    
   }
 
 })
