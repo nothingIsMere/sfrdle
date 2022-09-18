786cb909f1f259117d6274ac66a7b9da42686d11
Joel <joel.knoll1@gmail.com>
2022-09-18 01:29:49 -0400

    More refactoring/tinkering.

--- script-3.js
+++ script-3.js
@@ -3,7 +3,8 @@ let masterLetterArray = [];
 let gameOver = false;
 let currentSubmission = "";
 let offLimitsCount = 0; 
-let submissionCount = 0; 
+let submissionCount = 0;
+let letterCount = 0;  
 
 const letterboxNodeList = document.querySelectorAll(".letterbox");
 let letterboxArray = Array.from(letterboxNodeList);
@@ -15,61 +16,76 @@ const acceptableKeys = ["A", "B", "C", "D", "E", "F", "G",
 
 window.addEventListener("keydown", (e) => {
 
-if(!acceptableKeys.includes(e.key)){
-  return; 
-}
-else if(e.key === "Backspace"){
-  if(gameOver){
-    return;
+  if(!acceptableKeys.includes(e.key)){              //INVALID KEY
+    return; 
   }
-  if(masterLetterArray.length === offLimitsCount){
-    return;
-  }
-  masterLetterArray.pop();
-  letterboxArray[masterLetterArray.length].textContent = "";
-  letterboxArray[masterLetterArray.length].classList.remove("filled-letterbox");
-  console.log(`masterLetterArray.length = ${masterLetterArray.length}`);
-}
-else if(e.key === "Enter"){
-  if(gameOver){
-    return;
-  }
-  
-  if(masterLetterArray.length%5 != 0){
-    currentSubmission = "";
-    for(let i=masterLetterArray.length - masterLetterArray.length%5; i < masterLetterArray.length; i++){
-      currentSubmission += masterLetterArray[i];
+  else if(e.key === "Backspace"){                   //BACKSPACE
+    if(gameOver){
+      return;
     }
-    console.log(currentSubmission);
-  }
-  else if(masterLetterArray.length%5 === 0){
-    currentSubmission = "";
-    for(let i=masterLetterArray.length - 5; i < masterLetterArray.length; i++){
-      currentSubmission += masterLetterArray[i];
+    if(letterCount > 0){
+      letterCount -= 1;
     }
-    console.log(currentSubmission);
+    if(masterLetterArray.length === offLimitsCount){
+      return;
+    }
+    masterLetterArray.pop();
+    letterboxArray[masterLetterArray.length].textContent = "";
+    letterboxArray[masterLetterArray.length].classList.remove("filled-letterbox");
+    console.log(`masterLetterArray.length = ${masterLetterArray.length}`);
   }
+  else if(e.key === "Enter"){                         //ENTER
+    if(gameOver){
+      return;
+    }
+    
+    if(masterLetterArray.length%5 != 0){
+      currentSubmission = "";
+      for(let i=masterLetterArray.length - masterLetterArray.length%5; i < masterLetterArray.length; i++){
+        currentSubmission += masterLetterArray[i];
+      }
+      console.log(currentSubmission);
+    }
+    else if(masterLetterArray.length%5 === 0){
+      currentSubmission = "";
+      for(let i=masterLetterArray.length - 5; i < masterLetterArray.length; i++){
+        currentSubmission += masterLetterArray[i];
+      }
+      console.log(currentSubmission);
+    }
 
-  if(currentSubmission.length < 5 || masterLetterArray.length/5 == submissionCount){
-    alert("not enough letters");
-  }
-  else{
-    offLimitsCount = masterLetterArray.length;
-    submissionCount += 1; 
-  }
+    if(currentSubmission.length < 5 || masterLetterArray.length/5 == submissionCount){
+      alert("not enough letters");
+    }
+    else{
+      offLimitsCount = masterLetterArray.length;
+      submissionCount += 1;
+      letterCount = 0;  
+    }
 
-  console.log(`offLimitsCount = ${offLimitsCount}`);
-  console.log(`currentSubmission = ${currentSubmission}`);
-}
-else{
-  if(gameOver){
-    return;
+    console.log(`offLimitsCount = ${offLimitsCount}`);
+    console.log(`currentSubmission = ${currentSubmission}`);
+    console.log(`submissionCount = ${submissionCount}`);
   }
-  masterLetterArray.push(e.key);
-  letterboxArray[masterLetterArray.length - 1].textContent = e.key.toUpperCase();
-  letterboxArray[masterLetterArray.length - 1].classList.add("filled-letterbox");
+  else{                                          //VALID LETTER
+    if(gameOver){
+      return;
+    }
 
-  console.log(`masterLetterArray.length = ${masterLetterArray.length}`);
+    if(letterCount === 5){
+      return;
+    }
+    else{
+      masterLetterArray.push(e.key);
+      letterboxArray[masterLetterArray.length - 1].textContent = e.key.toUpperCase();
+      letterboxArray[masterLetterArray.length - 1].classList.add("filled-letterbox");
+
+      letterCount += 1;
+
+      console.log(`masterLetterArray.length = ${masterLetterArray.length}`);
+
+    }
+
 }
 
 
