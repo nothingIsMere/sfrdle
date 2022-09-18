1349302113c623faa58f28cc56f6fad85b4d55a0
Joel <joel.knoll1@gmail.com>
2022-09-17 21:15:00 -0400

    Specify how to bulid currentSubmission string for testing against various conditions.

--- script-3.js
+++ script-3.js
@@ -5,7 +5,6 @@ const acceptableKeys = ["A", "B", "C", "D", "E", "F", "G",
 "H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
 "t","u","v","w","x","y","z","Enter", "Backspace", "ENTER", "BACK"];
-let currentSubmission = "";
 let officialSubmissionCount = 0;   
 
 const letterboxNodeList = document.querySelectorAll(".letterbox");
@@ -21,12 +20,24 @@ window.addEventListener("keydown", (e) => {
     masterLetterList.pop();
     letterboxArray[masterLetterList.length].textContent = "";
     letterboxArray[masterLetterList.length].classList.remove("filled-letterbox"); 
-    
-    console.log(masterLetterList.length);
+
   }
-  else if(e.key === "Enter"){             //ENTER
+  else if(e.key === "Enter"){              //ENTER
+    let currentSubmission = "";
     if(gameOver){return;}
-    
+    if(masterLetterList.length%5 === 0){
+      for(let i = masterLetterList.length - 5; i < masterLetterList.length; i++){
+        currentSubmission += masterLetterList[i];
+      }
+    }
+    else{
+      for(let i = masterLetterList.length - masterLetterList.length%5;i<masterLetterList.length;i++){
+        currentSubmission += masterLetterList[i];
+      }
+    }
+  
+    console.log(currentSubmission);
+     
   }
   else{                                   //VALID LETTER
     if(gameOver){return;}
