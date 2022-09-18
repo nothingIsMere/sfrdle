1ee9fa78fe88f3310f2111efb2d311868305abee
Joel <joel.knoll1@gmail.com>
2022-09-18 00:56:21 -0400

    Refactoring script-3.js

--- script-3.js
+++ script-3.js
@@ -1,6 +1,8 @@
 let currentWord = "score";
 let masterLetterArray = []; 
 let gameOver = false;
+let currentSubmission = "";
+let offLimitsCount = 0; 
 
 const letterboxNodeList = document.querySelectorAll(".letterbox");
 let letterboxArray = Array.from(letterboxNodeList);
@@ -19,15 +21,42 @@ else if(e.key === "Backspace"){
   if(gameOver){
     return;
   }
+  if(masterLetterArray.length === offLimitsCount){
+    return;
+  }
   masterLetterArray.pop();
   letterboxArray[masterLetterArray.length].textContent = "";
   letterboxArray[masterLetterArray.length].classList.remove("filled-letterbox");
+  console.log(`masterLetterArray.length = ${masterLetterArray.length}`);
 }
 else if(e.key === "Enter"){
   if(gameOver){
     return;
   }
-  console.log(e.key);
+  
+  if(masterLetterArray.length%5 != 0){
+    currentSubmission = "";
+    for(let i=masterLetterArray.length - masterLetterArray.length%5; i < masterLetterArray.length; i++){
+      currentSubmission += masterLetterArray[i];
+    }
+    console.log(currentSubmission);
+  }
+  else if(masterLetterArray.length%5 === 0){
+    currentSubmission = "";
+    for(let i=masterLetterArray.length - 5; i < masterLetterArray.length; i++){
+      currentSubmission += masterLetterArray[i];
+    }
+    console.log(currentSubmission);
+  }
+
+  if(currentSubmission.length < 5){
+    alert("not enough letters");
+  }
+  else{
+    offLimitsCount = masterLetterArray.length;
+  }
+
+  console.log(`offLimitsCount = ${offLimitsCount}`);
 }
 else{
   if(gameOver){
@@ -36,6 +65,8 @@ else{
   masterLetterArray.push(e.key);
   letterboxArray[masterLetterArray.length - 1].textContent = e.key.toUpperCase();
   letterboxArray[masterLetterArray.length - 1].classList.add("filled-letterbox");
+
+  console.log(`masterLetterArray.length = ${masterLetterArray.length}`);
 }
 
 
