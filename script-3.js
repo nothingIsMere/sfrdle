2365a0c17edcd1666248e61d39a7b4fb9e1952e4
Joel <joel.knoll1@gmail.com>
2022-09-17 20:40:26 -0400

    More refactoring

--- script-3.js
+++ script-3.js
@@ -5,7 +5,10 @@ const acceptableKeys = ["A", "B", "C", "D", "E", "F", "G",
 "H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
 "t","u","v","w","x","y","z","Enter", "Backspace", "ENTER", "BACK"];
+let currentSubmission = "";  
 
+const letterboxNodeList = document.querySelectorAll(".letterbox");
+let letterboxArray = Array.from(letterboxNodeList);
 
 window.addEventListener("keydown", (e) => {
 
@@ -13,14 +16,23 @@ window.addEventListener("keydown", (e) => {
     return;
   }
   else if(e.key === "Backspace"){         //BACKSPACE
-
+    if(gameOver){return;}
+    masterLetterList.pop();
+    letterboxArray[masterLetterList.length].textContent = "";
+    letterboxArray[masterLetterList.length].classList.remove("filled-letterbox"); 
+    // console.log(masterLetterList);
   }
   else if(e.key === "Enter"){             //ENTER
-
+    if(gameOver){return;}
+    
   }
-  else(){                                 //VALID KEY
-
+  else{                                   //VALID LETTER
+    if(gameOver){return;}
+    masterLetterList.push(e.key);
+    letterboxArray[masterLetterList.length - 1].textContent = e.key.toUpperCase();
+    letterboxArray[masterLetterList.length - 1].classList.add("filled-letterbox");
+    // console.log(masterLetterList);
   }
 
 
-});
+})
