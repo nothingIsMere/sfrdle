b9768ea2045f54789ec31e4508180b56853935eb
Joel <joel.knoll1@gmail.com>
2022-09-17 21:03:33 -0400

    More work on script-3.js

--- script-3.js
+++ script-3.js
@@ -5,7 +5,8 @@ const acceptableKeys = ["A", "B", "C", "D", "E", "F", "G",
 "H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z",
 "a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s",
 "t","u","v","w","x","y","z","Enter", "Backspace", "ENTER", "BACK"];
-let currentSubmission = "";  
+let currentSubmission = "";
+let officialSubmissionCount = 0;   
 
 const letterboxNodeList = document.querySelectorAll(".letterbox");
 let letterboxArray = Array.from(letterboxNodeList);
@@ -20,7 +21,8 @@ window.addEventListener("keydown", (e) => {
     masterLetterList.pop();
     letterboxArray[masterLetterList.length].textContent = "";
     letterboxArray[masterLetterList.length].classList.remove("filled-letterbox"); 
-    // console.log(masterLetterList);
+    
+    console.log(masterLetterList.length);
   }
   else if(e.key === "Enter"){             //ENTER
     if(gameOver){return;}
@@ -31,7 +33,6 @@ window.addEventListener("keydown", (e) => {
     masterLetterList.push(e.key);
     letterboxArray[masterLetterList.length - 1].textContent = e.key.toUpperCase();
     letterboxArray[masterLetterList.length - 1].classList.add("filled-letterbox");
-    // console.log(masterLetterList);
   }
 
 
