4a31ad7ed46cfa6cfe9c7877c8a067a1af7d37a3
Joel <joel.knoll1@gmail.com>
2022-09-20 22:01:39 -0400

    Blergh

--- script-3.js
+++ script-3.js
@@ -76,13 +76,13 @@ let letterCount = 0;
 let isWord = true; 
 let successCount = 0;
 
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+// let vh = window.innerHeight * 0.01;
+// document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-});
+// window.addEventListener('resize', () => {
+//   let vh = window.innerHeight * 0.01;
+//   document.documentElement.style.setProperty('--vh', `${vh}px`);
+// });
 
 const modalWarning = document.getElementById("modal-warning"); 
 let warningText = document.getElementById("warning-text");
